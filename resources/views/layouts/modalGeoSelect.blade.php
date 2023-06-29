e98b87b807e93106a930e8cf70b033cc2ff1daf5
Lorean7 <oleglone7@gmail.com>
2023-06-30 00:02:42 +0300

    обновлен геоселект

--- resources/views/layouts/modalGeoSelect.blade.php
+++ resources/views/layouts/modalGeoSelect.blade.php
@@ -130,7 +130,7 @@ if (empty($_COOKIE['city'])){
                                     <ul class="geo-list__list-item">
                                         <?php foreach ($mainCity as $main) { ?>
                                             <li class="geo-list__item">
-                                                <a class="geo-list__link main" href="#">
+                                                <a class="geo-list__link main js-fancybox-close" href="#">
                                                     <svg
                                                         xmlns="http://www.w3.org/2000/svg"
                                                         width="24"
@@ -154,7 +154,7 @@ if (empty($_COOKIE['city'])){
                                     </ul>
                                 </li>
                                 <li class="geo-list__list-items">
-                                    <?php
+                                    <?php 
                                     // Сортируем массив $otherCity по полю "city" в алфавитном порядке
                                     usort($otherCity, function($a, $b) {
                                         return strcmp($a['city'], $b['city']);
@@ -162,46 +162,53 @@ if (empty($_COOKIE['city'])){
                                 
                                     $currentLetter = ''; // Переменная для хранения текущей буквы
                                 
-                                    foreach($otherCity as $other) {
+                                    foreach($otherCity as $key => $other) {
                                         $cityName = $other['city']; // Получаем название города из массива
                                 
                                         // Получаем первую букву названия города
                                         $firstLetter = mb_substr($cityName, 0, 1, 'UTF-8');
                                 
                                         // Если текущая буква отличается от предыдущей, выводим заголовок с новой буквой
-                                        if ($firstLetter !== $currentLetter) {
-                                            $currentLetter = $firstLetter;
-                                            echo '<div class="geo-list__list-title">' . $currentLetter . '</div>';
-                                        }
-                                    ?>
-                                    <ul class="geo-list__list-item">
-                                        <li class="geo-list__item">
-                                            <a class="geo-list__link" href="#">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="24"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    class="geo-list__icon"
-                                                >
-                                                    <path
-                                                        d="M14 7H16.3373C16.5818 7 16.7041 7 16.8192 7.02763C16.9213 7.05213 17.0188 7.09253 17.1083 7.14736C17.2092 7.2092 17.2957 7.29568 17.4686 7.46863L21.5314 11.5314C21.7043 11.7043 21.7908 11.7908 21.8526 11.8917C21.9075 11.9812 21.9479 12.0787 21.9724 12.1808C22 12.2959 22 12.4182 22 12.6627V15.5C22 15.9659 22 16.1989 21.9239 16.3827C21.8224 16.6277 21.6277 16.8224 21.3827 16.9239C21.1989 17 20.9659 17 20.5 17M15.5 17H14M14 17V7.2C14 6.0799 14 5.51984 13.782 5.09202C13.5903 4.71569 13.2843 4.40973 12.908 4.21799C12.4802 4 11.9201 4 10.8 4H5.2C4.0799 4 3.51984 4 3.09202 4.21799C2.71569 4.40973 2.40973 4.71569 2.21799 5.09202C2 5.51984 2 6.0799 2 7.2V15C2 16.1046 2.89543 17 4 17M14 17H10M10 17C10 18.6569 8.65685 20 7 20C5.34315 20 4 18.6569 4 17M10 17C10 15.3431 8.65685 14 7 14C5.34315 14 4 15.3431 4 17M20.5 17.5C20.5 18.8807 19.3807 20 18 20C16.6193 20 15.5 18.8807 15.5 17.5C15.5 16.1193 16.6193 15 18 15C19.3807 15 20.5 16.1193 20.5 17.5Z"
-                                                        stroke="black"
-                                                        stroke-width="2"
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                    ></path>
-                                                </svg>
-                                                <span class="geo-list__label"><?php echo $cityName; ?></span>
-                                            </a>
-                                        </li>
-                                    </ul>
+                                        if ($firstLetter != $currentLetter) {
+                                            $currentLetter = $firstLetter; ?> 
+                                            <div class="geo-list__list-title"><?= $currentLetter ?></div>
+                                            <ul class="geo-list__list-item">
+                                        <?php } ?>
+                                            <li class="geo-list__item">
+                                                <a class="geo-list__link js-fancybox-close" href="#">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="24"
+                                                        height="24"
+                                                        viewBox="0 0 24 24"
+                                                        fill="none"
+                                                        class="geo-list__icon"
+                                                    >
+                                                        <path
+                                                            d="M14 7H16.3373C16.5818 7 16.7041 7 16.8192 7.02763C16.9213 7.05213 17.0188 7.09253 17.1083 7.14736C17.2092 7.2092 17.2957 7.29568 17.4686 7.46863L21.5314 11.5314C21.7043 11.7043 21.7908 11.7908 21.8526 11.8917C21.9075 11.9812 21.9479 12.0787 21.9724 12.1808C22 12.2959 22 12.4182 22 12.6627V15.5C22 15.9659 22 16.1989 21.9239 16.3827C21.8224 16.6277 21.6277 16.8224 21.3827 16.9239C21.1989 17 20.9659 17 20.5 17M15.5 17H14M14 17V7.2C14 6.0799 14 5.51984 13.782 5.09202C13.5903 4.71569 13.2843 4.40973 12.908 4.21799C12.4802 4 11.9201 4 10.8 4H5.2C4.0799 4 3.51984 4 3.09202 4.21799C2.71569 4.40973 2.40973 4.71569 2.21799 5.09202C2 5.51984 2 6.0799 2 7.2V15C2 16.1046 2.89543 17 4 17M14 17H10M10 17C10 18.6569 8.65685 20 7 20C5.34315 20 4 18.6569 4 17M10 17C10 15.3431 8.65685 14 7 14C5.34315 14 4 15.3431 4 17M20.5 17.5C20.5 18.8807 19.3807 20 18 20C16.6193 20 15.5 18.8807 15.5 17.5C15.5 16.1193 16.6193 15 18 15C19.3807 15 20.5 16.1193 20.5 17.5Z"
+                                                            stroke="black"
+                                                            stroke-width="2"
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                        ></path>
+                                                    </svg>
+                                                    <span class="geo-list__label"><?=$cityName ?></span>
+                                                </a>
+                                            </li>
+                                        <?php
+                                        // Получаем следующую букву для следующего литерала
+                                        $nextLiteral = isset($otherCity[$key+1]) ? mb_substr($otherCity[$key+1]['city'], 0, 1, 'UTF-8') : '';
+                                
+                                        if ($nextLiteral != $currentLetter) {
+                                            // Если следующая буква отличается от текущей, закрываем текущий список
+                                            ?>
+                                            </ul>
+                                        <?php } ?>
                                     <?php } ?>
                                 </li>
                                 
-                            </ul>
-                        </div>
+                                </ul>
+                            </div>
                         <!-- end .geo-list-->
                     </div>
                 </div>
